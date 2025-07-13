5a4f9e591f2bf4df5d376652d1246d12ec684e1b
muhammadludviarg <116613247+muhammadludviarg@users.noreply.github.com>
2025-07-14 01:55:06 +0700

    Update Publikasi.php

--- app/Models/Publikasi.php
+++ app/Models/Publikasi.php
@@ -1,15 +1,18 @@
 <?php
-namespace App\Models; 
-use Illuminate\Database\Eloquent\Factories\HasFactory; 
-use Illuminate\Database\Eloquent\Model; 
-class Publikasi extends Model 
-{ 
-    use HasFactory; 
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory ;
+
+class Publikasi extends Model
+{
+     use HasFactory; 
  
     protected $fillable = [ 
         'title', 
         'releaseDate', 
         'description', 
         'coverUrl',     
-    ]; 
-} 
+    ];
+}
