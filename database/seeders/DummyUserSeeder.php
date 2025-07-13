1eef54eff0eaa3f6557a36dd741b9711e317d3c5
Your Name <you@example.com>
2025-07-13 20:24:18 +0700

    Initial commit of Laravel backend for Modul 14

---
+++ database/seeders/DummyUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User; // Pastikan ini diimpor
+use Illuminate\Support\Facades\Hash; // Pastikan ini diimpor
+
+class DummyUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password') // Ini akan menghasilkan hash bcrypt yang aman [cite: 222]
+        ]);
+    }
+}
