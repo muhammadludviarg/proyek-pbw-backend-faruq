c039022332da598027810ae8c3f3e831e55f99be
muhammadludviarg <116613247+muhammadludviarg@users.noreply.github.com>
2025-07-14 01:57:53 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,28 +2,31 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PublikasiController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PublikasiController;
 
-// Rute yang menggunakan middleware 'web' untuk session/CSRF (Login dan Logout)
-// Ini adalah praktik yang lebih kuat untuk SPA dengan Sanctum
-Route::group(['middleware' => ['web']], function () {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout']); // Logout di sini
-});
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-// Rute yang memerlukan autentikasi dengan Sanctum token
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function () {
-    // Rute untuk mendapatkan data user yang sedang login
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-    // Rute untuk Publikasi
+    // Publikasi
     Route::get('/publikasi', [PublikasiController::class, 'index']);
     Route::post('/publikasi', [PublikasiController::class, 'store']);
-    Route::get('/publikasi/{id}', [PublikasiController::class, 'show']);
-    Route::patch('/publikasi/{id}', [PublikasiController::class, 'update']); // Menggunakan PATCH untuk update
-    Route::put('/publikasi/{id}', [PublikasiController::class, 'change']); // Sesuai kode Anda, jika 'change' berbeda dari 'update'
-    Route::delete('/publikasi/{id}', [PublikasiController::class, 'destroy']);
+    // Logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+    // Publikasi detail
+    Route::get('/publikasi/{publikasi}', [PublikasiController::class, 'show']);
+    Route::put('/publikasi/{publikasi}', [PublikasiController::class,'update']);
+    Route::delete('/publikasi/{publikasi}', [PublikasiController::class, 'destroy']);
 });
+
