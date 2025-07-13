a41dbdd1c8b22eee3b53ee18f7c6fd4fc51e011f
muhammadludviarg <116613247+muhammadludviarg@users.noreply.github.com>
2025-07-13 23:02:24 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,8 +2,7 @@
 return [ 
 'paths' => ['api/*'], 
 'allowed_methods' => ['*'], 
-'allowed_origins' => ['http://localhost:5173', 'https://web-bps
-frontend.vercel.app'], 
+'allowed_origins' => ['http://localhost:5173', 'https://proyek-pbw-frontend-faruq.vercel.app'], 
 'allowed_origins_patterns' => [], 
 'allowed_headers' => ['*'], 
 'exposed_headers' => [], 
