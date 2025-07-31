dc99ef674f1dcb7f23414350b91f67eb5cdf5449
muhammadludviarg <116613247+muhammadludviarg@users.noreply.github.com>
2025-07-31 21:27:28 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,6 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'], // Pastikan sanctum/csrf-cookie ada di sini
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'http://localhost:5173', // Untuk development lokal
         'https://proyek-pbw-frontend-faruq.vercel.app', // GANTI DENGAN URL VERCEL ANDA YANG SEBENARNYA
     ],
     'allowed_origins_patterns' => [],
